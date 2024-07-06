410d7e7d53acb99aec362a65239577c56064080c
Quentin Ng <ng.quentin@gmail.com>
2024-07-06 18:01:59 +1000

    Remove bin and obj directory

--- src/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ src/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89767b27f4f10a3c1b4511f21ef3fabafe4a1bd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e324451e5492329d1388baec140a7da6d3d740")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
